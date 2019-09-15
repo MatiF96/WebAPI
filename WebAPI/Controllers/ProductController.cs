4d50808db6e016f3292633ced83e928eeca5fd49
M F <matif96@o2.pl>
2019-09-15 22:59:14 +0200

    Dodaj pliki projektów.

---
+++ WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/Product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductContext _context;
+
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+
+            if (_context.Products.Count() == 0)
+            {
+                // Create a default Product if collection is empty,
+                // which means you can't delete all Products.
+                _context.Products.Add(new Product { Id = new Guid("00000000-0000-0000-0000-000000000001"), Name = "DefaultName1", Price = 0.00m });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> Get()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> Get(Guid id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> Post(Product item)
+        {
+            _context.Products.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(Guid id, Product item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ WebAPI/wwwroot/site.js
@@ -0,0 +1,148 @@
+﻿const uri = "api/Product";
+let todos = null;
+function getCount(data) {
+    const el = $("#counter");
+    let name = "product";
+    if (data) {
+        if (data > 1) {
+            name = "products";
+        }
+        el.text(data + " " + name);
+    } else {
+        el.text("No " + name);
+    }
+}
+
+$(document).ready(function () {
+    getData();
+});
+
+function getData() {
+    $.ajax({
+        type: "GET",
+        url: uri,
+        cache: false,
+        success: function (data) {
+            const tBody = $("#todos");
+
+            $(tBody).empty();
+
+            getCount(data.length);
+
+            $.each(data, function (key, item) {
+                const tr = $("<tr></tr>")
+                    .append($("<td></td>").text(item.id))
+                    .append($("<td></td>").text(item.name))
+                    .append($("<td></td>").text(item.price))
+                    .append(
+                        $("<td></td>").append(
+                            $("<button>Edit</button>").on("click", function () {
+                                editItem(item.id);
+                            })
+                        )
+                    )
+                    .append(
+                        $("<td></td>").append(
+                            $("<button>Delete</button>").on("click", function () {
+                                deleteItem(item.id);
+                            })
+                        )
+                    );
+
+                tr.appendTo(tBody);
+            });
+
+            todos = data;
+        }
+    });
+}
+
+function addItem() {
+
+    const item = {
+        name: $("#add-name").val(),
+        price: $("#add-price").val()
+    };
+    if (item.name) {
+        if (item.name.length <= 100) {
+            $.ajax({
+                type: "POST",
+                accepts: "application/json",
+                url: uri,
+                contentType: "application/json",
+                data: JSON.stringify(item),
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert("Something went wrong!");
+                },
+                success: function (result) {
+                    getData();
+                    $("#add-name").val("");
+                    $("#add-price").val("0.00");
+                }
+            });
+        }
+        else {
+            alert("Entered name can't have more than 100 characters!");
+        }
+    }
+    else {
+        alert("Enter product name!");
+    }
+}
+
+function deleteItem(id) {
+    $.ajax({
+        url: uri + "/" + id,
+        type: "DELETE",
+        success: function (result) {
+            getData();
+        }
+    });
+}
+
+function editItem(id) {
+    $.each(todos, function (key, item) {
+        if (item.id === id) {
+            $("#edit-id").val(item.id);
+            $("#edit-name").val(item.name);
+            $("#edit-price").val(item.price);
+        }
+    });
+    $("#spoiler").css({ display: "block" });
+}
+
+$(".my-form").on("submit", function () {
+    const item = {
+        id: $("#edit-id").val(),
+        name: $("#edit-name").val(),
+        price: $("#edit-price").val()
+    };
+
+    if (item.id) {
+        if (item.name.length <= 100) {
+            $.ajax({
+                url: uri + "/" + $("#edit-id").val(),
+                type: "PUT",
+                accepts: "application/json",
+                contentType: "application/json",
+                data: JSON.stringify(item),
+                success: function (result) {
+                    getData();
+                }
+            });
+        }
+        else {
+            alert("Entered name can't have more than 100 characters!");
+        }
+    }
+
+    else {
+        alert("Id missing!");
+    }
+    closeInput();
+    return false;
+});
+
+function closeInput() {
+    $("#spoiler").css({ display: "none" });
+}
